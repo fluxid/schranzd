ab72788eeb587b27f917834c3e398e2472ec83b7
Fluxid <accreg@fluxid.pl>
2010-07-11 19:53:00 +0200

    * Better error handling in mail module

--- src/schranz/modules/mail.py
+++ src/schranz/modules/mail.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import os.path
 import subprocess
 
@@ -42,17 +43,17 @@ class MailConfigParser(SimpleParser):
     def parse_account(self, account):
         tmp = account.split('@')
         if len(tmp) != 2:
-            raise ParseError('Malformed account name')
+            raise ParseError('Malformed account name.')
         
         user, domain = tmp
         if user.find('-') != -1:
-            raise ParseError('Username cannot contain "-" (dash) character')
+            raise ParseError('Username cannot contain "-" (dash) character.')
 
         if user in FORBIDDEN_USERS:
-            raise ParseError('You cannot use username "%s"' % user)
+            raise ParseError('You cannot use username "%s".' % user)
 
         if domain not in self.domains:
-            raise ParseError('Unknown domain "%s"' % domain)
+            raise ParseError('Unknown domain "%s".' % domain)
 
     def cmd_account(self, account, password):
         self.parse_account(account)
@@ -67,25 +68,25 @@ class MailConfigParser(SimpleParser):
         self.parse_account(account1)
 
         if account1 in self.accounts:
-            raise ParseError('Account "%s" already exists' % account1)
+            raise ParseError('Account "%s" already exists.' % account1)
         
         alias = self.aliases.get(account2)
         if alias:
             account2 = alias
         elif account2 not in self.accounts:
-            raise ParseError('Unknown account "%s"' % account2)
+            raise ParseError('Unknown account "%s".' % account2)
         
         self.aliases[account1] = account2
 
     def cmd_catchall(self, domain, account):
         if domain not in self.domains:
-            raise ParseError('Unknown domain "%s"' % domain)
+            raise ParseError('Unknown domain "%s".' % domain)
 
         alias = self.aliases.get(account)
         if alias:
             account = alias
         elif account not in self.accounts:
-            raise ParseError('Unknown account "%s"' % account)
+            raise ParseError('Unknown account "%s".' % account)
 
         self.catchalls[domain] = account
 
@@ -94,15 +95,11 @@ def mail_reload(context, arguments):
     errors = context['response']['errors']
     user = context['user']
 
-    config_fp = os.path.join(user.pw_dir, CONFIG_FILE)
-    if not os.path.isfile(config_fp):
-        errors.append('No configuration file exists')
-
     domains = set()
 
     domain_fp = os.path.join(DOMAIN_PATH, user.pw_name)
     if not os.path.isfile(domain_fp):
-        errors.append('No domain file exists')
+        errors.append('Domain file for your user does not exist.')
     else:
         f = open(domain_fp, 'r')
         for line in f.xreadlines():
@@ -114,9 +111,11 @@ def mail_reload(context, arguments):
 
     if not errors:
         parser = MailConfigParser(domains)
-        parser.parse(config_fp)
-        # must use +=, or else we would have to replace errors from context
-        errors += parser.errors
+        try:
+            parser.parse(os.path.join(user.pw_dir, CONFIG_FILE))
+            errors += parser.errors
+        except EnvironmentError, e:
+            errors.append('Error when parsing configuration: %s.' % os.strerror(e.errno))
 
     if not errors:
         passwords = dict()
@@ -124,7 +123,7 @@ def mail_reload(context, arguments):
             try:
                 p = subprocess.Popen([DOVECOTPW_BIN, '-p', value], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
             except:
-                errors.append('Error when hashing password for account "%s"' % key)
+                errors.append('Error when hashing password for account "%s".' % key)
                 continue
             
             pstdout, _ = p.communicate()
